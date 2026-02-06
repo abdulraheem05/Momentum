31ab14f1926ef560d73a599fe1027f7e8f262ae2
Abdul Raheem <abdlraheem26@gmail.com>
2026-02-06 17:55:36 +0530

    wav audio extraction logic

--- backend/services/ffmpeg_utils.py
+++ backend/services/ffmpeg_utils.py
@@ -9,4 +9,13 @@ def run_ffmpeg(args: list[str]) -> None:
         raise RuntimeError(proc.stderr.strip() or "ffmpeg failed")
     
 def extract_audio_wav (video_path : Path, audio_out: Path) -> None:
-    audio_out.parent.mkdir()
+    audio_out.parent.mkdir(parents=True, exist_ok=True)
+
+    run_ffmpeg([
+        "-i", str(video_path),
+        "-vn",
+        "-ac", "1",
+        "-ar", "16000",
+        "-f", "wav"    
+    ])
+
