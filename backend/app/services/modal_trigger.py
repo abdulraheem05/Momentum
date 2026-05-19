19c39060e283d2b0c964e8a2d5d6a904a5e1a57c
Abdul Raheem <abdlraheem26@gmail.com>
2026-05-19 16:39:34 +0530

    Refactored modal trigger

--- backend/app/services/modal_trigger.py
+++ backend/app/services/modal_trigger.py
@@ -1,10 +1,37 @@
 import modal
 
+
 process_video = modal.Function.from_name(
     "momentum-worker",
     "process_video"
 )
 
+search_scenes_modal = modal.Function.from_name(
+    "momentum-worker",
+    "search_scenes"
+)
 
-def trigger_worker(job_id, payload):
-    process_video.spawn(job_id, payload)
+search_audio_modal = modal.Function.from_name(
+    "momentum-worker",
+    "search_audio"
+)
+
+
+async def trigger_worker(job_id, payload):
+    await process_video.spawn.aio(job_id, payload)
+
+
+async def run_scene_search(job_id, query):
+    return await search_scenes_modal.remote.aio(
+        job_id,
+        query,
+        5
+    )
+
+
+async def run_audio_search(job_id, query):
+    return await search_audio_modal.remote.aio(
+        job_id,
+        query,
+        5
+    )
